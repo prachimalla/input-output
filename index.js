a8c6a0cf901de95d3ee3fbf5c2ddf975cafccb06
prachimalla <prachimalla7@gmail.com>
2022-07-29 12:59:25 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-var arr1 = "john".split('');
+var arr1 = 'john'.split('');
 var arr2 = arr1.reverse();
-console.log(arr2)
-var arr3 = "jones".split('');
+console.log(arr2);
+var arr3 = 'jones'.split('');
 arr2.push(arr3);
-console.log(arr2,arr1)
-console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
-console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+console.log(arr2, arr1);
+console.log('array 1: length=' + arr1.length + ' last=' + arr1.slice(-1));
+console.log('array 2: length=' + arr2.length + ' last=' + arr2.slice(-1));
+
+console.log(19 + 'a' - '1');
+var a = (1, 2, 3);
+console.log(a);
+
+console.log(typeof NaN);
+
+var Employee = {
+  company: 'xyz',
+};
+var emp1 = Employee;
+// delete emp1.company
+console.log(emp1.company);
